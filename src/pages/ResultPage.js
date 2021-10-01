ba21449e666d8a745e48c1c43e1322e650cb125c
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-10-01 14:43:27 +0300

    taken output wşth context and sent to Pagination component as a prop

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
 import Pagination from "../components/Pagination";
+import Card from "../components/Card";
+import { engineContext } from "../context/engineContext";
+import { useContext } from "react";
 
-const ResultPage = (props) => {
+const ResultPage = () => {
+
+    const {data,output} = useContext(engineContext)
+
   const history = useHistory();
   const goMain = () => {
     history.push("/");
@@ -10,14 +16,18 @@ const ResultPage = (props) => {
 
 
   // Pagination
-  const [cards, setCards] = useState("");
+
   const [currentPage, setCurrentPage] = useState(1);
   const [cardPerPage, setCardPerPage] = useState(3);
 
-  
   const indexOfLastCard = currentPage * cardPerPage;
-  const indexOfFirstCard = indexOfLastCard - cardPerPage;
-  const currentCards = cards.slice(indexOfFirstCard, indexOfLastCard)
+  const indexofFirstCard = indexOfLastCard- cardPerPage;
+  const currentCards = output.slice(indexofFirstCard, indexOfLastCard);
+//   console.log(currentCards);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <div className="d-flex flex-column">
@@ -31,9 +41,17 @@ const ResultPage = (props) => {
         </button>
       </div>
 
-      <div className="output col-md-10">{props.location.output}</div>
+      <div className="output col-md-10">
+        {currentCards.map((item, index) => (
+          <Card key={index} item={item} />
+        ))}
+      </div>
 
-      <Pagination cards={currentCards} />
+      <Pagination
+        paginate={paginate}
+        output={output}
+        cardPerPage={cardPerPage}
+      />
     </div>
   );
 };
