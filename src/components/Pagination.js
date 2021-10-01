4a93d00fa90f3666cb1868a9ff591d206c381198
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-10-01 14:43:52 +0300

    taken output with destruction and used in paginate funct.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const Pagination = ({cards}) => {
+
+
+const Pagination = ({paginate,output,cardPerPage}) => {
+    
+    
+    const pageNumbers = [];
+
+    for(let i = 1; i <= Math.ceil(output.length / cardPerPage); i++){
+        pageNumbers.push(i)
+    }
+
+
     return (
-        <div>
-            
-        </div>
+        <nav>
+        <ul className = "pagination d-flex justify-content-center mt-3">
+            {pageNumbers.map(number => (
+                <li key = {number} className = "page-item">
+                    <a  onClick={() => paginate(number)} href = "#" className = "page-link bg-secondary text-light h5 m-1">
+                        {number}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </nav>
     )
 }
 
-export default Pagination
+export default Pagination;
